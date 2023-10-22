b6d913b7cc64e808e514e63ac94c1bec52fda6b2
Arif Yakupogullari <2637106y@student.gla.ac.uk>
2023-10-22 17:19:14 +0100

    Created the video trimming tool using ffmpeg.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,14 @@ def trim(in_file, out_file,start,end):
     probe_result = ffmpeg.probe(in_file)
     duration = probe_result.get("format",{}).get("duration",None)
     print(duration)
-
+    input_stream = ffmpeg.input(in_file)
+    pts = "PTS-STARTPTS"
+    video = input_stream.trim(start=start, end=end).setpts(pts)
+    audio = (input_stream
+             .filter_("atrim", start=start, end=end)
+             .filter_("asetpts", pts))
+    video_and_audio = ffmpeg.concat(video,audio,v=1,a=1)
+    output = ffmpeg.output(video_and_audio, out_file, format = "mp4")
+    output.run()
 
 trim("movie.mp4","out.mp4",10,18)
