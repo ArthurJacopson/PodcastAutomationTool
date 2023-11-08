c76382eedc36c2b46b24d8b588b6db8e10bfa3d4
Boris Velinov <2641360v@student.gla.ac.uk>
2023-11-08 11:15:01 +0000

    Resolve "Fix navbar styling"

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,21 +4,17 @@ import './Navbar.css'
 import rectangle from '../media/Rectangle-2.png'
 import podplistic from '../media/podplistic.png'
 import profile from '../media/account.png'
-import Title from './Title'
 
 const Navbar: React.FC<{ title: string }> = ({ title }) => {
     const navigate = useNavigate();
     const handleClick = () => navigate('/');
 
     return (
-        <div>
-            <div className="navBackground">
-                <img className="navBackgroundLogo" alt="Logo" src={podplistic} onClick={handleClick} />
-                <img className="navBackgroundRectangle" alt="Rectangle" src={rectangle} />
-                <Title text={title} />
+            <div className="navContainer">
+                <img className="navLogo" alt="Logo" src={podplistic} onClick={handleClick} />
+                <h1 className="TitleText">{  title }</h1>
                 <img className='navBackgroundAccount' alt="Account" src={profile} />
             </div>
-        </div>
     );
 };
 
