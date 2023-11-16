88606e3502127f675761e5ce8ca22981f722b4a2
Ewan Hibberd <2640046h@student.gla.ac.uk>
2023-11-16 17:00:04 +0000

    Reworked FileComponent unit tests to be more gooder

--- frontend/src/components/FileComponent.test.tsx
+++ frontend/src/components/FileComponent.test.tsx
@@ -1,31 +1,50 @@
-import renderer, { ReactTestRenderer, ReactTestRendererJSON } from 'react-test-renderer';
-import FileComponent from './FileComponent';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from 'history';
+import FileComponent from "./FileComponent";
 
-it('renders the component', () => { 
 
-    const sampleData = { slug: "a", name: "Project A", date: "Oct 25, 2023", size: 55.71 }
 
-    const routes = [
+test('correct stuff is shown in component', () => {
+
+    const sampleData = { 
+        slug: "a", 
+        name: "Project A",
+        date: "Oct 25, 2023",
+        size: 55.71 
+    }
+    const history = createMemoryHistory();
+    history.push = jest.fn();
+
+    render(
+        <Router location={history.location} navigator={history}>
+            <FileComponent 
+                slug={sampleData.slug}
+                name={sampleData.name}
+                date={sampleData.date}
+                size={sampleData.size}
+            />
+        </Router>
+    )
+
+    expect(screen.getByText('Project A')).toBeInTheDocument();
+    expect(screen.getByText('Project size: 55.71')).toBeInTheDocument();
+    
+    fireEvent.click(screen.getByText(/Project A/i));
+    expect(history.push).toHaveBeenCalledWith(
         {
-            path: '/test',
-            element: <FileComponent 
-                        slug={sampleData.slug} 
-                        name={sampleData.name} 
-                        date={sampleData.date}
-                        size={sampleData.size}
-                    />,
+            hash: '',
+            pathname: '/editor/a',
+            search: '',
+            preventScrollReset: undefined,
         },
-    ];
+        undefined,
+        {
+            preventScrollReset: undefined,
+            relative: undefined,
+            replace: false,
+            state: undefined,
+        }
+    );
 
-    const router = createMemoryRouter(routes, {
-        initialEntries: ['/test'],
-        initialIndex: 0,
-    });
-
-    const component = renderer.create(
-        <RouterProvider router={router} />
-    ) as ReactTestRenderer;
-    let tree = component.toJSON() as ReactTestRendererJSON;
-    expect(tree).toMatchSnapshot();
 });
