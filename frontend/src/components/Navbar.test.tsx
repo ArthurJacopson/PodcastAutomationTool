dbcc949f3b1b41470032a60d32b89eae28e2a84b
Ewan Hibberd <2640046h@student.gla.ac.uk>
2023-11-17 14:41:31 +0000

    Removed unneeded asynchronicity from Navbar tests

--- frontend/src/components/Navbar.test.tsx
+++ frontend/src/components/Navbar.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { MemoryRouter, useNavigate }  from "react-router-dom";
+import { MemoryRouter, }  from "react-router-dom";
 import Navbar from "./Navbar";
 
 const mockNavigate = jest.fn();
@@ -10,7 +10,7 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockNavigate,
 }));
 
-test('if the navbar loads correctly and routes to the correct places', async () => {
+test('if the navbar loads correctly and routes to the correct places', () => {
     render(
         <MemoryRouter>
             <Navbar title={"test title"}/>
@@ -20,7 +20,7 @@ test('if the navbar loads correctly and routes to the correct places', async () 
 
     expect(screen.getByRole('heading')).toHaveTextContent('test title');
 
-    await userEvent.click(screen.getByAltText("Logo"));
+    userEvent.click(screen.getByAltText("Logo"));
 
     expect(mockNavigate).toHaveBeenCalledWith('/');
 
