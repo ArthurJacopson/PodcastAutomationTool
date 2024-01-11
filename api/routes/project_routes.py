6254fdd034b5dea9a27ad6b6d8f27217fb973472
Jamie Robb <jamierobb321@gmail.com>
2024-01-11 17:56:06 +0000

    Added new route to create project and removed test-db-connection route

--- api/routes/project_routes.py
+++ api/routes/project_routes.py
@@ -4,28 +4,8 @@ from db import db
 
 from api.models.project import Project
 
-
-
 bp = Blueprint('project_routes', __name__)
 
-@bp.route('/test-db-connection', methods=['GET'])
-def test_db_connection():
-    """
-    Helper function to test the database connection. This will be deleted from the final repo.
-    """
-    try:
-        new_project = Project(name='TEST PROJECT NAME!!')
-        db.session.add(new_project)
-        db.session.commit()
-        project = Project.query.first()
-    
-    except Exception as e:
-        error_message = f'Error connecting to the database: {str(e)}'
-        return jsonify({'error': error_message}), 500
-
-    else:
-        result = {'message': 'Database connection successful', 'project_id': project.project_id, 'name': project.name}
-        return jsonify(result), 200
 
 @bp.route('/projects', methods=['GET'])
 def get_projects():
@@ -40,11 +20,23 @@ def get_projects():
         error_message = f'Error retrieving projects: {str(e)}'
         return jsonify({'error': error_message}), 500
 
-# @bp.route('/create/<project_name>', methods=['POST'])
-# def create_project(project_name):
-#     data_str = request.data.decode('utf-8')
-#     data = json.loads(data_str)
-#     newProject = Project(data['slug'], data['name'], data['date'], data['size'])
-#     projects.insert(0, newProject)
-#     response = "success"
-#     return response
+
+@bp.route('/create/<project_name>', methods=['POST'])
+def create_project(project_name):
+    """
+    Endpoint to be used when creating a new project.
+    :param project_name: the name of the project to be created.
+    """
+    try:
+        new_project = Project(name=project_name)
+        db.session.add(new_project)
+        db.session.commit()
+        project = Project.query.first()
+    
+    except Exception as e:
+        error_message = f'Error connecting to the database: {str(e)}'
+        return jsonify({'error': error_message}), 500
+
+    else:
+        result = {'message': 'Database connection successful', 'project_id': project.project_id, 'name': project.name}
+        return jsonify(result), 200
